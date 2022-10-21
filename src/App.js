4e2867f22335c1756fdb956571544c7a77da1c93
caiosantosss <caio.santos@msn.com>
2022-10-21 22:36:58 +0900

    add browserRouter and activeMenu

--- src/App.js
+++ src/App.js
@@ -6,18 +6,34 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
+
+  const activeMenu = true;
+
   return (
-    <div className="flex relative dark:bg-main-dark-bg">
-      <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
-        <TooltipComponent content='Settings' position='Top' showTipPointer={false}>
-          <button type='button'
-          className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-          style={{ background: 'blue', borderRadius: '50%' }}>
-            {/* the style is separated because we are going to change it later */}
-            <FiSettings />
-          </button>
-        </TooltipComponent>
-      </div>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
+            <TooltipComponent content='Settings' position='Top' showTipPointer={false}>
+              <button type='button'
+              className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+              style={{ background: 'blue', borderRadius: '50%' }}>
+                {/* the style is separated because we are going to change it later */}
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div>
+              Sidebar
+            </div>
+          ) : (
+            <div>
+              Sidebar w-0
+            </div>
+          )}
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
