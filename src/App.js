daeea46eef41071ec3403a194922d98133df4378
caiosantosss <caio.santos@msn.com>
2022-10-23 20:58:58 +0900

    sidebar component

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
             </div>
           ) : (
             <div className='w-0 dark:bg-secondary-dark-bg'>
-                <Sidebar />
+              <Sidebar />
             </div>
           )}
           <div className={
