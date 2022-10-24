b876cc55f43aa59ce4edd8951ea45d574f8e49ec
caiosantosss <caio.santos@msn.com>
2022-10-24 19:48:49 +0900

    add contex provider to app

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettigs } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Line, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
+import { useStateContext } from '../contexts/ContextProvider';
+
 import './App.css'
 
 const App = () => {
-
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
