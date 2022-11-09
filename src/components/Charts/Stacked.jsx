5e21f73711fea8b300e07b1f05d5380f17a98a19
caiosantosss <caio.santos@msn.com>
2022-11-09 22:34:40 +0900

    add stacked component chart

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Category, Legend, Tooltip, StackingColumnSeries } from '@syncfusion/ej2-react-charts';
 
-const Stacked = () => {
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+
+const Stacked = ({ width, height }) => {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      id="charts"
+      width={width}
+      height={height}
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: 'white' }}
+      title="Sales Analysis"
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective
+            key={index}
+            {...item}
+          />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   )
 }
 
