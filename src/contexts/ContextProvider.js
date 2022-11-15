a14c4e2e92c6ebab5d3517caf79ced4240110dcc
caiosantosss <caio.santos@msn.com>
2022-11-15 22:06:37 +0900

    add themeSettings, and add usestate at contextprovider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,20 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('light');
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+
+    localStorage.setItem('themeMode', e.target.value);
+  };
+
+  const setColor = (e) => {
+    setCurrentColor(e.target.value);
+
+    localStorage.setItem('colorMode', e.target.value);
+  };
 
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
@@ -24,6 +38,9 @@ export const ContextProvider = ({ children }) => {
       isClicked, setIsClicked,
       handleClick,
       screenSize, setScreenSize,
+      currentColor, setCurrentColor,
+      currentMode, setCurrentMode,
+      setMode, setColor,
     }}
     >
       {children}
