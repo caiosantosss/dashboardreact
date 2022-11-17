a625231edea773aca250cfb130f12229a09c597c
caiosantosss <caio.santos@msn.com>
2022-11-17 23:03:36 +0900

    add AreaChart

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, SplineAreaSeries, Category, Zoom, Crosshair } from '@syncfusion/ej2-react-charts';
+
+import { areaCustomSeries, areaPrimaryYAxis, areaPrimaryXAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import { Header } from '../../components';
 
 const Area = () => {
+  const { currentMode } = useStateContext();
+
   return (
-    <div>Area</div>
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+      <Header category='Area' title='Inflation Rate in Percentage' />
+      <ChartComponent
+        id='area-charts'
+        height='420px'
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      >
+        <Inject services={[SplineAreaSeries, Category, DateTime, Legend, Zoom, Crosshair]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
   )
 }
 
